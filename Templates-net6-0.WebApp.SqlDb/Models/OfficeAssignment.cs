57e136bebef3bc3c13211d7acd2a587379e00a00
David Rachwalik <david.rachwalik@outlook.com>
2021-11-25 14:47:03 -0600

    minor format touchups

--- Templates-net6-0.WebApp.SqlDb/Models/OfficeAssignment.cs
+++ Templates-net6-0.WebApp.SqlDb/Models/OfficeAssignment.cs
@@ -7,6 +7,7 @@ public class OfficeAssignment
 {
     [Key]
     public int InstructorID { get; set; }
+
     [StringLength(50)]
     [Display(Name = "Office Location")]
     public string Location { get; set; }
