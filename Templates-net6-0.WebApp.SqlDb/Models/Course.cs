eaef0fbbd3e17b9d9afbb8481e23d3deba84706d
David Rachwalik <david.rachwalik@outlook.com>
2021-11-27 02:18:37 -0600

    abstracted Course model base

--- Templates-net6-0.WebApp.SqlDb/Models/Course.cs
+++ Templates-net6-0.WebApp.SqlDb/Models/Course.cs
@@ -3,18 +3,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Templates_net6_0.WebApp.SqlDb.Models;
 
-public class Course
+public class CourseBase
 {
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     [Display(Name = "Number")]
     public int CourseID { get; set; }
 
     [StringLength(50, MinimumLength = 3)]
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
 
     [Range(0, 5)]
     public int Credits { get; set; }
+}
 
+public class CourseDTO : CourseBase
+{
+}
+
+public class Course : CourseBase
+{
     public int? DepartmentID { get; set; }
 
     public Department? Department { get; set; }
--- Templates-net6-0.WebApp.SqlDb/Models/ViewModels/CourseVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Templates_net6_0.WebApp.SqlDb.Models.ViewModels;
-
-public class CourseVM
-{
-    public int CourseID { get; set; }
-    public string Title { get; set; }
-    public int Credits { get; set; }
-    public string DepartmentName { get; set; }
-}
